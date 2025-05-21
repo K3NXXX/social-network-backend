8f9d612ce9d1daa73a83094cf45d93622cd6f3fa
Pavlov Denys <116432682+07pm@users.noreply.github.com>
2025-05-21 21:54:06 +0300

    added user.service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, UserService, PrismaService],
 })
 export class UserModule {}
