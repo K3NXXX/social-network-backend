f9da20410cdc5e1a887d28f6bce0015cac18e0aa
idenyy <d3nys.pavlov@gmail.com>
2025-05-21 17:19:58 +0300

    refactor main app, extend Prisma schema, and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
     origin: ['*'],
     credentials: true,
   });
-  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(config.getOrThrow<number>('PORT'));
 }
