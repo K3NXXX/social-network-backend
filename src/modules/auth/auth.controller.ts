e2ff4b983638372f9c2522913844139973d98493
idenyy <d3nys.pavlov@gmail.com>
2025-05-22 10:36:52 +0300

    enhance token handling in auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,16 +4,19 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
   Res,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, SignupDto } from './dto/auth.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { Authorization } from '../../common/decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
+
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   async register(
@@ -21,12 +24,12 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     const { refreshToken, ...response } = await this.authService.register(dto);
-    
+
     this.authService.addRefreshToken(res, refreshToken);
-    
+
     return response;
   }
-  
+
   @Post('login')
   @HttpCode(HttpStatus.CREATED)
   async login(
@@ -34,21 +37,36 @@ export class AuthController {
     @Res({ passthrough: true }) res: Response,
   ) {
     const { refreshToken, ...response } = await this.authService.login(dto);
-    
+
     this.authService.addRefreshToken(res, refreshToken);
-    
+
     return response;
   }
-  
+
   @Post('logout')
   async logout(@Res({ passthrough: true }) res: Response) {
     this.authService.removeRefreshToken(res);
     return true;
   }
-  
+
   @Post('refresh')
+  @Authorization()
   @HttpCode(HttpStatus.CREATED)
-  async refresh(@Body('refreshToken') refreshToken: string) {
-    return this.authService.refresh(refreshToken);
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = req.cookies['refreshToken'];
+    if (!refreshToken) throw new UnauthorizedException('Refresh token missing');
+
+    const {
+      user,
+      accessToken,
+      refreshToken: newRefreshToken,
+    } = await this.authService.refresh(refreshToken);
+
+    this.authService.addRefreshToken(res, newRefreshToken);
+
+    return { accessToken, user };
   }
 }
