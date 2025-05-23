1d76c0794020e74ca73b9b6ba9c11bacda98df7e
idenyy <d3nys.pavlov@gmail.com>
2025-05-23 15:41:18 +0300

    add follower and following endpoints to UserController

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -12,15 +12,31 @@ import { Authorization } from 'src/common/decorators/auth.decorator';
 import { CurrentUser } from 'src/common/decorators/user.decorator';
 import { UpdateUserDto } from './dto/user.dto';
 import { UserService } from './user.service';
+import { FollowService } from '../follow/follow.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly followService: FollowService,
+  ) {}
 
   @Authorization()
   @Get()
   async getProfile(@CurrentUser('id') userId: string) {
     return this.userService.findById(userId);
+  }
+
+  @Authorization()
+  @Get('followers')
+  async getMyFollowers(@CurrentUser('id') userId: string) {
+    return this.followService.getFollowers(userId);
+  }
+
+  @Authorization()
+  @Get('following')
+  getFollowing(@CurrentUser('id') userId: string) {
+    return this.followService.getFollowing(userId);
   }
 
   @Authorization()
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/common/prisma.service';
-import { UserController } from './user.controller'
-import { CloudinaryService } from '../cloudinary/cloudinary.service'
+import { UserController } from './user.controller';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { FollowService } from '../follow/follow.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService, CloudinaryService],
+  providers: [UserService, PrismaService, CloudinaryService, FollowService],
 })
 export class UserModule {}
