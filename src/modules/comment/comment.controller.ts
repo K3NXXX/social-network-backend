19931d252faad56395160954068e62f029c37665
idenyy <d3nys.pavlov@gmail.com>
2025-06-01 21:18:36 +0300

    feat: add pagination support for fetching post comments and comment replies

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,11 +1,12 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Query,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { Authorization } from '../../common/decorators/auth.decorator';
@@ -14,42 +15,50 @@ import { CommentDto, UpdateCommentDto } from './dto/comment.dto';
 
 @Controller('comments')
 export class CommentController {
-  constructor(private readonly commentService: CommentService) {}
+	constructor(private readonly commentService: CommentService) {}
 
-  @Post()
-  @Authorization()
-  createComment(@Body() dto: CommentDto, @CurrentUser('id') userId: string) {
-    return this.commentService.create(dto, userId);
-  }
+	@Post()
+	@Authorization()
+	createComment(@Body() dto: CommentDto, @CurrentUser('id') userId: string) {
+		return this.commentService.create(dto, userId);
+	}
 
-  @Get('post/:postId')
-  findAllForPost(@Param('postId') postId: string) {
-    return this.commentService.findAllForPost(postId);
-  }
+	@Get('post/:postId')
+	findAllForPost(
+		@Param('postId') postId: string,
+		@Query('page') page = '1',
+		@Query('take') take = '15',
+	) {
+		return this.commentService.findAllForPost(postId, +page, +take);
+	}
 
-  @Get(':id/replies')
-  findReplies(@Param('id') id: string) {
-    return this.commentService.findReplies(id);
-  }
+	@Get(':id/replies')
+	findReplies(
+		@Param('id') id: string,
+		@Query('page') page = '1',
+		@Query('take') take = '5',
+	) {
+		return this.commentService.findReplies(id, +page, +take);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(id);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.commentService.findOne(id);
+	}
 
-  @Patch(':id')
-  @Authorization()
-  update(
-    @Param('id') id: string,
-    @Body() dto: UpdateCommentDto,
-    @CurrentUser('id') userId: string,
-  ) {
-    return this.commentService.update(id, userId, dto.content as string);
-  }
+	@Patch(':id')
+	@Authorization()
+	update(
+		@Param('id') id: string,
+		@Body() dto: UpdateCommentDto,
+		@CurrentUser('id') userId: string,
+	) {
+		return this.commentService.update(id, userId, dto.content as string);
+	}
 
-  @Delete(':id')
-  @Authorization()
-  remove(@Param('id') id: string, @CurrentUser('id') userId: string) {
-    return this.commentService.remove(id, userId);
-  }
+	@Delete(':id')
+	@Authorization()
+	remove(@Param('id') id: string, @CurrentUser('id') userId: string) {
+		return this.commentService.remove(id, userId);
+	}
 }
