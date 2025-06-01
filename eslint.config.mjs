50d4e71df58cfd8fac2f84588561b28a610d7a84
idenyy <d3nys.pavlov@gmail.com>
2025-06-01 23:17:49 +0300

    refactor: update ESLint configuration for improved compatibility with TypeScript and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,48 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-<<<<<<< HEAD
-  plugins: ['@typescript-eslint', '@typescript-eslint/eslint-plugin',],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'prettier'
-  ],
-  env: {
-    node: true,
-    jest: true,
-=======
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
->>>>>>> 1a4a9c694734123ddd23690f782d858a48689c69
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+	{
+		ignores: ['eslint.config.mjs'],
+	},
+	{
+		plugins: ['@typescript-eslint', '@typescript-eslint/eslint-plugin'],
+		extends: [
+			'eslint:recommended',
+			'plugin:@typescript-eslint/recommended',
+			'plugin:prettier/recommended',
+			'plugin:react/recommended',
+			'prettier',
+		],
+		env: {
+			node: true,
+			jest: true,
+		},
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+			sourceType: 'module',
+			parserOptions: {
+				project: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-floating-promises': 'warn',
+			'@typescript-eslint/no-unsafe-argument': 'warn',
+			'prettier/prettier': 'off',
+		},
+	},
 );
