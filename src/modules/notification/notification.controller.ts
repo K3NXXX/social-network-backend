623c345c34528d072cb3fd120fc0f6bbdfaf1523
idenyy <d3nys.pavlov@gmail.com>
2025-06-17 20:51:45 +0300

    refactor: update notification controller and service to include a new endpoint for marking individual notifications as read, and streamline method signatures for consistency

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { CurrentUser } from '../../common/decorators/user.decorator';
 import { Authorization } from '../../common/decorators/auth.decorator';
 
 @Controller('user')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+	constructor(private readonly notificationService: NotificationService) {}
 
-  @Authorization()
-  @Get('notifications')
-  getForUser(@CurrentUser('id') userId: string) {
-    return this.notificationService.getUserNotifications(userId);
-  }
+	@Authorization()
+	@Get('notifications')
+	getForUser(@CurrentUser('id') userId: string) {
+		return this.notificationService.getUserNotifications(userId);
+	}
 
-  @Authorization()
-  @Get('notifications/readAll')
-  async markAllAsRead(@CurrentUser('id') userId: string) {
-    await this.notificationService.markAllAsRead(userId);
-    return { success: true };
-  }
+	@Authorization()
+	@Get('notifications/read')
+	async markAllAsRead(@CurrentUser('id') userId: string) {
+		await this.notificationService.markAllAsRead(userId);
+		return { success: true };
+	}
+
+	@Authorization()
+	@Get('notification/:id/read')
+	async markAsRead(@Param('id') notificationId: string) {
+		await this.notificationService.markAsRead(notificationId);
+		return { success: true };
+	}
 }
--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,60 +1,69 @@
-
 import { Injectable } from '@nestjs/common';
-import { CreateNotificationDto } from './dto/notification.dto'
-import { PrismaService } from 'src/common/prisma.service'
+import { CreateNotificationDto } from './dto/notification.dto';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
 export class NotificationService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-  async create(dto: CreateNotificationDto) {
-    return this.prisma.notification.create({
-      data: {
-        ...dto,
-      },
-    });
-  }
+	async create(dto: CreateNotificationDto) {
+		return this.prisma.notification.create({
+			data: {
+				...dto,
+			},
+		});
+	}
 
- async getUserNotifications(userId: string) {
-  return this.prisma.notification.findMany({
-    where: { userId },
-    orderBy: { createdAt: 'desc' },
-    select: {
-      id: true,
-      type: true,
-      message: true,
-      isRead: true,
-      createdAt: true,
-      sender: {
-        select: {
-          id: true,
-          username: true,
-          firstName: true,
-          lastName: true,
-          avatarUrl: true
-        },
-      },
-      post: {
-        select: {
-          id: true,
-          content: true,
-          photo: true,
-        },
-      },
-    },
-  });
-}
+	async getUserNotifications(userId: string) {
+		return this.prisma.notification.findMany({
+			where: { userId },
+			orderBy: { createdAt: 'desc' },
+			select: {
+				id: true,
+				type: true,
+				message: true,
+				isRead: true,
+				createdAt: true,
+				sender: {
+					select: {
+						id: true,
+						username: true,
+						firstName: true,
+						lastName: true,
+						avatarUrl: true,
+					},
+				},
+				post: {
+					select: {
+						id: true,
+						content: true,
+						photo: true,
+					},
+				},
+			},
+		});
+	}
 
+	async markAllAsRead(userId: string) {
+		return this.prisma.notification.updateMany({
+			where: {
+				userId,
+				isRead: false,
+			},
+			data: {
+				isRead: true,
+			},
+		});
+	}
 
-async markAllAsRead(userId: string) {
-  return this.prisma.notification.updateMany({
-    where: {
-      userId,
-      isRead: false,
-    },
-    data: {
-      isRead: true,
-    },
-  });
-}
+	async markAsRead(notificationId: string) {
+		return this.prisma.notification.update({
+			where: {
+				id: notificationId,
+			},
+			data: {
+				isRead: true,
+			},
+		});
+	}
 }
